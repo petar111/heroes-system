a2f338b00d898889faf7942ece92dfcf26ee7a07
Petar Jeremic <jpetar765@gmail.com>
2024-11-07 13:06:54 +0100

    Added stats calculcation

--- code/sample/heroesstatsmanagerservice/heroesstatsmanagerservice/app/app.py
+++ code/sample/heroesstatsmanagerservice/heroesstatsmanagerservice/app/app.py
@@ -1,14 +1,35 @@
+import numpy as np
 from flask import Flask
 import json
 import os
+import numpy
+import http.client
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello/message')
 def hello():
     x = {'message':'Hello world',
          'osName':os.name}
     return x
+
+@app.route('/stats/integer/mean')
+def calculate_integer_mean():
+    client = http.client.HTTPConnection(host='reactive-java', port=8081)
+    client.request('GET', '/stats/sampledata/integer/1000')
+    response = client.getresponse()
+
+
+    raw_data = response.read()
+    data = json.loads(raw_data)
+
+    return {'size': len(data),
+            'stats':{
+                'mean': float(np.mean(data)),
+                'sum': float(np.sum(data)),
+                'min': float(np.min(data)),
+                'max': float(np.max(data))
+            }}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- code/sample/reactivejava/src/main/java/com/example/reactivejava/controller/StatsConsumerController.java
+++ code/sample/reactivejava/src/main/java/com/example/reactivejava/controller/StatsConsumerController.java
@@ -2,10 +2,15 @@ package com.example.reactivejava.controller;
 
 import com.example.reactivejava.model.StatsMessageDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/stats")
@@ -14,8 +19,23 @@ public class StatsConsumerController {
     @GetMapping("/message")
     public Mono<StatsMessageDto> statsMessage(){
 
-        WebClient client = WebClient.create("http://stats-producer:5000/");
-        return client.get().exchangeToMono(r -> r.bodyToMono(StatsMessageDto.class));
+        WebClient client = WebClient.create("http://stats-producer:5000");
+        return client.get().uri("/hello/message")
+                .exchangeToMono(r -> r.bodyToMono(StatsMessageDto.class));
+    }
+
+    @GetMapping("/sampledata/integer/{n}")
+    public Mono<List<Integer>> sampleIntegers(@PathVariable int n){
+        Random r = new Random();
+
+        List<Integer> result = new ArrayList<>(2000);
+
+        for(int i = 0; i < n; i++){
+            result.add(r.nextInt(1000));
+        }
+
+        return Mono.just(result);
+
     }
 
     @GetMapping("/hello")
